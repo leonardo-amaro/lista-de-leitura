fc0d57c510db2c9469e8bd3244eaac876a276cd3
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2024-11-21 14:57:12 -0300

    criar servidor http

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Servidor rodando...');
+const http = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const Server = http.createServer((req, res) => {
+  const file = req.url === "/" ? "index.html" : req.url;
+  const filePath = path.join(__dirname, "public", file);
+  const extname = path.extname(filePath);
+  const extValidas = [".html", ".css", ".js"];
+  const validos = extValidas.find((item) => item == extname);
+
+  if (!validos) return;
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) throw err;
+    res.end(content);
+  });
+});
+
+Server.listen(5000, () => console.log('Servidor rodando...'));
