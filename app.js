55da6df1d5c04db83c2f2825d6225f3f8a43aed4
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2024-12-05 17:11:52 -0300

    Rota principal e integração com db

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const Conexao = require('./db.js');
 
 const App = express();
+
+App.use('/css', express.static('./public/css'));
 
 App.engine('handlebars', engine());
 App.set('view engine', 'handlebars');
 App.set('views', './views');
 
+Conexao.connect();
+
 App.get('/', (req, res) => {
-  res.render('home');
+  Conexao.query(
+    'SELECT * FROM livros',
+    (erro, retorno) => {
+      if (erro) throw erro;
+      res.render('home', { livros: retorno });
+    }
+  );
 });
 
 App.listen(8080, () => console.log('Servidor rodando...'));
